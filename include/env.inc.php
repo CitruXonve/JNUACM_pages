5b36029a08df20be948b691fb242715232b7a28c
Xu Zongdi <semprathlon@163.com>
2016-02-25 19:45:56 +0800

    remove private config

--- include/env.inc.php
+++
@@ -1,8 +0,0 @@
-<?php $cfg=array (
-  'data_server' => 'localhost',
-  'data_database' => 'bd_db',
-  'data_uid' => 'bd_user',
-  'data_pwd' => 'c111',
-//  'dir_root' => 'cogs/',
-//  'testdata' => '/home/cogs/testdata',
-) ?>
